60a7f9103ec455271274dbcc35f77806e6cdd50a
Mike Snacki <mikesnacki@gmail.com>
2019-07-07 14:16:58 -0400

    deleted extraneous configs in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,6 @@ module.exports = {
         rules: [
             { test: /\.(js)$/, use: 'babel-loader' },
             { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-        ],
-        loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: [/node_modules/] }
         ]
     },
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
